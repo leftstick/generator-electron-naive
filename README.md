deca03dcd204adf59ea4361db96a1e7846cb3fbd
Howard.Zuo <leftstick@qq.com>
2015-11-23 13:33:23 +0800

    init version of electron-naive

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # generator-electron-naive
+==================
+
+[![NPM version][npm-image]][npm-url]
+![][david-url]
+
 Create desktop app for a website in one minute
+
+
+## Prerequisites ##
+
+1. Install [yeoman](http://yeoman.io/): `npm install -g yo`
+2. Install __this__: `npm install -g generator-electron-naive`
+
+> prepend with `sudo` if you are running on `*nix` OS.
+
+## Use `generator-electron-naive` ##
+
+`yo electron-naive`
+
+> Answer questions as following demonstrated
+
+![](https://raw.githubusercontent.com/leftstick/generator-electron-naive/master/docs/img/questions.png)
+
+
+## generate your app ##
+
+Once your project is created by this `generator`, go into the project folder
+
+Run `npm start` to generate the executable file.
+
+> The file will be generated into `dist` folder
+
+## References ##
+
+1. [yeoman](http://yeoman.io/)
+2. [gulp](http://gulpjs.com/)
+3. [electron](http://electron.atom.io/)
+
+
+## LICENSE ##
+
+[MIT License](https://raw.githubusercontent.com/leftstick/generator-electron-naive/master/LICENSE)
+
+
+
+
+[npm-url]: https://npmjs.org/package/generator-electron-naive
+[npm-image]: https://badge.fury.io/js/generator-electron-naive.png
+[david-url]: https://david-dm.org/leftstick/generator-electron-naive.png
---
+++ generators/app/index.js
@@ -0,0 +1,136 @@
+'use strict';
+
+var generators = require('yeoman-generator');
+
+var gen = generators.Base.extend({
+    initializing: function() {
+
+        try {
+            this.username = process.env.USER || process.env.USERPROFILE.split(require('path').sep)[2];
+        } catch (e) {
+            this.username = '';
+        }
+    },
+    prompting: function() {
+        var done = this.async();
+        var self = this;
+
+        this.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Your project name',
+                validate: function(name) {
+                    if (!name) {
+                        return 'Project name cannot be empty';
+                    }
+                    if (!/\w+/.test(name)) {
+                        return 'Project name should only consist of 0~9, a~z, A~Z, _, .';
+                    }
+
+                    var fs = require('fs');
+                    if (!fs.existsSync(self.destinationPath(name))) {
+                        return true;
+                    }
+                    if (require('fs').statSync(self.destinationPath(name)).isDirectory()) {
+                        return 'Project already exist';
+                    }
+                    return true;
+                }
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Your project description',
+                default: ''
+            },
+            {
+                type: 'input',
+                name: 'username',
+                message: 'Your name',
+                default: this.username
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Your email',
+                default: ''
+            },
+            {
+                type: 'input',
+                name: 'webUrl',
+                message: 'Your website to be packaged',
+                validate: function(webUrl) {
+                    return !!webUrl;
+                }
+            },
+            {
+                type: 'checkbox',
+                name: 'platforms',
+                message: 'Which platform you\'d like to package to?',
+                choices: [
+                    'darwin',
+                    'darwin-x64',
+                    'win32',
+                    'win32-ia32',
+                    'win64-64',
+                    'linux',
+                    'linux-ia32',
+                    'linux-x64'
+                ],
+                validate: function(platforms) {
+                    return platforms.length > 0;
+                }
+            },
+            {
+                type: 'list',
+                name: 'registry',
+                message: 'Which registry would you use?',
+                choices: [
+                    'https://registry.npm.taobao.org',
+                    'https://registry.npmjs.org'
+                ]
+            }
+        ], function(answers) {
+            require('date-util');
+            this.answers = answers;
+            this.answers.date = new Date().format('mmm d, yyyy');
+            this.obj = {answers: this.answers};
+            done();
+        }.bind(this));
+    },
+    configuring: function() {
+        var path = require('path');
+        var fs = require('fs');
+        var self = this;
+        var done = this.async();
+        fs.exists(this.destinationPath(this.answers.name), function(exists) {
+            if (exists && fs.statSync(self.destinationPath(self.answers.name)).isDirectory()) {
+                self.log.error('Directory [' + self.answers.name + '] exists');
+                process.exit(1);
+            }
+            self.destinationRoot(path.join(self.destinationRoot(), self.answers.name));
+            done();
+        });
+    },
+    writing: function() {
+        var self = this;
+
+        self.copy(self.templatePath('gitignore'), self.destinationPath('.gitignore'));
+        self.fs.copyTpl(self.templatePath('gulpfile.js.vm'), self.destinationPath('gulpfile.js'), self.obj);
+        self.fs.copyTpl(self.templatePath('main.js.vm'), self.destinationPath('src/main.js'), self.obj);
+        self.fs.copyTpl(self.templatePath('package.json.vm'), self.destinationPath('package.json'), self.obj);
+        self.fs.copyTpl(self.templatePath('src-package.json.vm'), self.destinationPath('src/package.json'), self.obj);
+    },
+    install: function() {
+        this.npmInstall(['gulp','rimraf','gulp-electron'], {
+            registry: this.answers.registry,
+            saveDev: true
+        });
+    },
+    end: function() {
+        this.log.ok('Project ' + this.answers.name + ' generated!!!');
+    }
+});
+
+module.exports = gen;
